cdc9516f7e8e7aeb2953ce4f3d74717232aa5bd4
Chimera64000 <Chimera64000@gmail.com>
2025-02-07 00:57:22 -0500

    Finished Lesson 3.1 - Jump Force

---
+++ Units/Sound and Effects/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Player jumps with spacebar and IsOnGround is set to false
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    //Sets isOnGround to true once the player touches the ground and collides with the ground
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+}
