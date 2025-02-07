dc727d470d8b2f0e0e6f25773f7a3c253765598a
Lizmarie-Quiles-Olivero <33010555@sfcollege.edu>
2025-02-06 19:27:31 -0500

    Finished Challenge 2 - Play Fetch

--- Challenges/Basic Gameplay/Challenge 2 - Play Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Challenges/Basic Gameplay/Challenge 2 - Play Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Hierarchy;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float cooldown = 0;
 
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        // On spacebar press, send dog and sets a cool down
+        if (Input.GetKeyDown(KeyCode.Space) && cooldown <= 0)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            cooldown = 1;
+        }
+        if (cooldown >= 0)
+        {
+            cooldown -= Time.deltaTime;
         }
     }
 }
