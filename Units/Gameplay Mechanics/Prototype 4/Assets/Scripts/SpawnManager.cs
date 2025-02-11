a83d4b607900322ccadcd75c4f70f28f24dcf5af
Chimera64000 <Chimera64000@gmail.com>
2025-02-11 01:05:55 -0500

    Finished Lesson 4.3 - PowerUp and CountDown

--- Units/Gameplay Mechanics/Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Units/Gameplay Mechanics/Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-
     public GameObject enemyPrefab;
     private float spawnRange = 9;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
