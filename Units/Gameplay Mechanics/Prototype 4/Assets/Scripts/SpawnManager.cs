577e4f6d6913743be06cfa846d40e9c128533fc6
Chimera64000 <Chimera64000@gmail.com>
2025-02-11 01:27:38 -0500

    Finished Lesson 4.4 - For-Loops For Waves

--- Units/Gameplay Mechanics/Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Units/Gameplay Mechanics/Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -3,20 +3,41 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
-    private float spawnRange = 9;
+    public GameObject powerupPrefab;
+    private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int waveNumber = 1;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        SpawnPowerupWave();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length;
+
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            SpawnPowerupWave();
+        }
     }
 
+    //Spawns an amount of enemies at the same time
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+    
+    //Randomize the spawn location of the enemies
     private Vector3 GenerateSpawnPosition()
     {
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
@@ -25,5 +46,10 @@ public class SpawnManager : MonoBehaviour
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
 
         return randomPos;
+    }
+
+    void SpawnPowerupWave()
+    {
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
     }
 }
