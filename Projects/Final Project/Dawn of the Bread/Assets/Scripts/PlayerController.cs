d57cbe3a3942219e9513fd16002226e229325b36
Lizmarie-Quiles-Olivero <33010555@sfcollege.edu>
2025-04-03 19:52:35 -0400

    Added basic player control

--- Projects/Final Project/Dawn of the Bread/Assets/Scripts/PlayerController.cs
+++ Projects/Final Project/Dawn of the Bread/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Public variables
+    public float horizontalInput;
+
+    //Private variables
+    private float speed = 25.0f;
+    private float xRange = 25;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Player movement
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        //Keeps player in bounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
     }
 }
