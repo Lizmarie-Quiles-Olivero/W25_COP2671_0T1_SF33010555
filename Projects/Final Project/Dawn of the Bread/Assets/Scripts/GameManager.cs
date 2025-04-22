bfe265e7dd40e9f384fa6d171729c5cc053db6c4
Lizmarie-Quiles-Olivero <33010555@sfcollege.edu>
2025-04-22 01:57:36 -0400

    Added Game Manager and cleaned up the amount of scripts being used

---
+++ Projects/Final Project/Dawn of the Bread/Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    //Public variables
+    public GameObject[] enemyPrefabs;
+    public ParticleSystem explosionParticle;
+    public AudioClip explosionSound;
+
+    //Private Variables
+    private float spawnRangeX = 25;
+    private float spawnPosZ = 40;
+    private float spawnRate = 4.0f;
+    private AudioSource enemyAudio;
+
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        enemyAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Spawns enemies randomly in a set area
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int enemyIndex = Random.Range(0, enemyPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            Instantiate(enemyPrefabs[enemyIndex], spawnPos, enemyPrefabs[enemyIndex].transform.rotation);
+        }
+    }
+
+    //Destroy zombie if collided with a meat
+    void OnTriggerEnter(Collider other)
+    {
+        if (CompareTag("Zombie"))
+        {
+            if (other.CompareTag("Meat"))
+            {
+                explosionParticle.Play();
+                AudioSource.PlayClipAtPoint(explosionSound, Camera.main.transform.position, 0.5f);
+                Destroy(gameObject, 0.05f);
+                Destroy(other.gameObject, 0.05f);
+            }
+        }
+
+        //Destroy alien if collided with a can
+        if (CompareTag("Alien"))
+        {
+            if (other.CompareTag("Can"))
+            {
+                explosionParticle.Play();
+                AudioSource.PlayClipAtPoint(explosionSound, Camera.main.transform.position, 0.5f);
+                Destroy(gameObject, 0.05f);
+                Destroy(other.gameObject, 0.05f);
+            }
+        }
+    }
+}
